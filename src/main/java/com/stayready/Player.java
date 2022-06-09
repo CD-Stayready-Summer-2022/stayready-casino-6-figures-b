efa8127c5a2f1a94915820373dfdb3de1eeb870a
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-09 16:35:42 -0400

    getter tests

--- src/main/java/com/stayready/Player.java
+++ src/main/java/com/stayready/Player.java
@@ -2,5 +2,41 @@ package com.stayready;
 
 public class Player {
     private String name;
-    
+    private Integer age;
+    private Double money;
+
+    public Player(String name, Integer age, Double money) {
+        this.name = name;
+        this.age = age;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return name+" "+age+" "+money;
+    }
 }
--- src/test/java/classtests/PlayerTest.java
+++ src/test/java/classtests/PlayerTest.java
@@ -1,4 +1,68 @@
 package classtests;
 
+
+import com.stayready.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+//import java.util.Scanner;
+
 public class PlayerTest {
+    @Test
+    @DisplayName("constructor test")
+    public void constructorTest00(){
+        Player player = new Player("",0,0.00);
+
+        Player actual = player;
+
+        Assertions.assertNotNull(actual);
+
+    }
+
+    @Test
+    @DisplayName("constructor test")
+    public void constructorTest01(){
+        Player expected = new Player("Sabrina",20,500.00);
+
+        String name = "Sabrina";
+        Integer age = 20;
+        Double money = 500.00;
+
+        Player player = new Player(name,age,money);
+
+        Player actual = player;
+
+        Assertions.assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    @DisplayName("getter test")
+    public void getterTest00(){
+        Player expected = new Player("Sabrina",20,500.00);
+
+        Player player = new Player("Sabrina", 20, 500.00);
+        String name = player.getName();
+        Integer age = player.getAge();
+        Double money = player.getMoney();
+
+        Player actual = player;
+
+        Assertions.assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    @DisplayName("setter test")
+    public void setterTest00(){
+        Player expected = new Player("Sabrina",21,1500.00);
+
+        Player player = new Player("Sabina", 20, 500.00);
+        String name = player.getName();
+        Integer age = player.getAge();
+        Double money = player.getMoney();
+
+        Player actual = player;
+
+        Assertions.assertEquals(expected.toString(), actual.toString());
+    }
 }
