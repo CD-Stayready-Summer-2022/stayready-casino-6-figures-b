790cde3c6e417e7dfb687ba5c67313252d3a0a8e
davidupnext <dadeleke31@gmail.com>
2022-06-09 16:30:53 -0400

    Card class

--- src/main/java/com/stayready/Card.java
+++ src/main/java/com/stayready/Card.java
@@ -1,4 +1,27 @@
 package com.stayready;
 
 public class Card {
+    private String suit;
+    private Integer value;
+
+    public Card(String suit, Integer value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void seValue(Integer value) {
+        this.value = value;
+    }
 }
