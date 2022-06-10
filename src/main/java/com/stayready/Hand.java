8f3fcf7d053ef4d5051fb17ce49a5744ef76bf12
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-10 08:58:30 -0400

    working on hand

--- src/main/java/com/stayready/Hand.java
+++ src/main/java/com/stayready/Hand.java
@@ -1,4 +1,21 @@
 package com.stayready;
 
-public class Hand {
+import java.util.ArrayList;
+import com.stayready.Card;
+
+public class Hand extends Deck {
+    private ArrayList<Card> cards;
+    private int cardIndex;
+
+
+    public void addCard(){
+
+        //cards.add(new Card(suit, Integer value));
+
+    }
+
+    public void removeCard(int cardIndex){
+        this.cardIndex = cardIndex;
+        cards.remove(cardIndex);
+    }
 }
