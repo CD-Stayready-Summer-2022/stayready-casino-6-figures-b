1d9aa58704bda97506655070f420e98d1bd0b5ae
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-10 09:15:04 -0400

    updated hand class

--- src/main/java/com/stayready/Hand.java
+++ src/main/java/com/stayready/Hand.java
@@ -1,17 +1,14 @@
 package com.stayready;
 
 import java.util.ArrayList;
-import com.stayready.Card;
 
 public class Hand extends Deck {
     private ArrayList<Card> cards;
     private int cardIndex;
 
 
-    public void addCard(){
-
-        //cards.add(new Card(suit, Integer value));
-
+    public void addCard(String suit, Integer value){
+        cards.add(new Card(suit, value));
     }
 
     public void removeCard(int cardIndex){
